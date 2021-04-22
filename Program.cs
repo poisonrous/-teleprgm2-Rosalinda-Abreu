4a5306adb333ddcc1679f23387832875fc603961
Rosalinda <abreuparrarosalinda@gmail.com>
2021-04-22 14:47:43 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Progra2!");
+        }
+    }
+}
